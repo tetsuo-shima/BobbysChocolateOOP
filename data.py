cb2a965849a7611df0c0233ff7e92652386ce515
Dwight Francis <dwight.l.francis@gmail.com>
2023-11-14 10:07:53 -0500

    change typehint in _cast_integers()

--- data.py
+++ data.py
@@ -4,14 +4,12 @@ from constants import ORDER_FILE, BONUS_PROMOTION_ACTIVATED
 from orderdata import OrderData
 
 
-# TODO: add tests
-
 def _read_csv(datafile=ORDER_FILE):
     with open(datafile, 'r') as file:
         return [order for order in csv.DictReader(file)]
 
 
-def _cast_integers(orders: dict):
+def _cast_integers(orders: list):
     new_orders = copy.deepcopy(orders)
     for order in new_orders:
         for key, value in order.items():
